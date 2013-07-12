f13274a2ba3cde9c0d31064a99ff73cc9db56ed7
DrizzitT <erictyoung@gmail.com>
2013-07-12 05:37:43 +0000

    Final?

--- web.js
+++ web.js
@@ -7,11 +7,10 @@ var fs = require('fs');
 var text = fs.readFileSync('index.html').toString('utf-8');
 
 app.get('/', function(request, response) {
-  response.send(console.log(text));
+  response.send(text);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
-  console.log(text);
 });
