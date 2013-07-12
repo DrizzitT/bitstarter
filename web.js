148d97478415755d5b6cce85d1948fdc376f0398
DrizzitT <erictyoung@gmail.com>
2013-07-12 02:51:40 +0000

    Fixed typo maybe 2?

--- web.js
+++ web.js
@@ -2,9 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buffer = new Buffer("Test", "utf-8")
+
 fs.readFileSync('/bitstarter/index.html', function (err, data){
  if (err) throw err;
-  var buffer = new Buffer(data);
+  buffer.write(data, "utf-8");
 });
  
 app.get('/', function(request, response) {
