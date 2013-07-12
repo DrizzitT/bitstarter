0831fa5c3eacebf741664a210d8608cb2fd3adad
DrizzitT <erictyoung@gmail.com>
2013-07-12 05:32:07 +0000

    test1

--- web.js
+++ web.js
@@ -1,4 +1,3 @@
-
 var express = require('express');
 
 var app = express.createServer(express.logger());
@@ -8,10 +7,11 @@ var fs = require('fs');
 var text = fs.readFileSync('/home/ubuntu/bitstarter/index.html').toString('utf-8');
 
 app.get('/', function(request, response) {
-  response.send(text);
+  response.send(console.log(text));
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
+  console.log(text);
 });
